200fb5c06e425696fe25d0451f6d840e29bb8011
Christopher Sardegna <github@reagentx.net>
2018-12-23 19:35:40 -0800

    Fix some javascript bugs

--- js/main.js
+++ js/main.js
@@ -28,9 +28,11 @@ $(window).load(function() {
     "use strict"; 
 
     //LOADER
-    $("#background").delay(1000).fadeOut(800);
-    $(".spinner").delay(500).fadeOut("slow");
-    $(".frame-loading").delay(500).fadeOut("slow");
+    /*
+    $("#background").delay(0).fadeOut(0);
+    $(".spinner").delay(0).fadeOut("slow");
+    $(".frame-loading").delay(0).fadeOut("slow");
+    */
     
         
 });
@@ -201,76 +203,10 @@ $(document).ready(function() {
     //CUSTOM SCROLLBAR
     $(".scrll").perfectScrollbar({
 
-        wheelSpeed:0.35,
+        wheelSpeed:0.55,
         suppressScrollX:true
    
     });
-
-
-    //CONTACT FORM
-    $("input,textarea").jqBootstrapValidation({
-        
-        preventSubmit: true,
-        submitError: function($form, event, errors) {
-        // something to have when submit produces an error ?
-        // Not decided if I need it yet
-        },
-        submitSuccess: function($form, event) {
-        event.preventDefault(); // prevent default submit behaviour
-        // get values from FORM
-        var name = $("input#name").val();  
-        var email = $("input#email").val(); 
-        var message = $("textarea#message").val();
-        var firstName = name; // For Success/Failure Message
-        // Check for white space in name for Success/Fail message
-        if (firstName.indexOf(' ') >= 0) {
-        firstName = name.split(' ').slice(0, -1).join(' ');
-          }        
-        $.ajax({
-                url: "./js/mailer.php",
-                type: "POST",
-                data: {name: name, email: email, message: message},
-                cache: false,
-               
-                success: function() {  
-                // Success message
-                $('#success').html("<div class='alert alert-success'>");
-                $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;").append( "</button>");
-                $('#success > .alert-success').append("Your message has been sent. ");
-                $('#success > .alert-success').append('</div>');
-                            
-                //clear all fields
-                $('#contactForm').trigger("reset");
-                },
-               
-                error: function() {      
-                // Fail message
-                $('#success').html("<div class='alert alert-danger'>");
-                $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;").append( "</button>");
-                $('#success > .alert-danger').append("Sorry "+firstName+" it seems that my mail server is not responding");
-                $('#success > .alert-danger').append('</div>');
-         
-                //clear all fields
-                $('#contactForm').trigger("reset");
-                },
-                })
-                },
-        
-                filter: function() {
-                return $(this).is(":visible");
-                },
-                });
-
-        $("a[data-toggle=\"tab\"]").on("click", function(e) {
-                e.preventDefault();
-                $(this).tab("show");
-        });
- 
-    $('#name').on("focus", function() {
-    $('#success').html('');
-    
-    });
-
 
     //IE SCROLL BUG FIX
     if(navigator.userAgent.match(/Trident\/7\./)) {
