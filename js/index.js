79eaf1aafd6571c79db40624167a346dd3530627
Christopher Sardegna <christopher.sardegna@cigna.com>
2019-10-10 10:22:01 -0400

    Resolve error where Typer.text is null

--- js/index.js
+++ js/index.js
@@ -82,7 +82,7 @@ function t()
 {
     Typer.addText({ keyCode: 123748 });
 
-    if (Typer.index > Typer.text.length)
+    if (Typer.text & Typer.index > Typer.text.length)
     {
         clearInterval(timer);
     }
